cbd15ceaea0cfe3e6654cc573763c69d0a814607
anil0701 <105093214+anil0701@users.noreply.github.com>
2022-10-27 14:18:09 +0530

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,11 @@
+function validate(){
+    var a=document.getElementById("username").value;
+    if(username=="admin"){
+        alert("iam loading ");
+        window.location.href="Date.html"
+    }
+    else{
+        alert("mismatch");
+    }
+
+}
